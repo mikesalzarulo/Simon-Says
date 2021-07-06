6070f02a47bc103d3267786627498b13421ee9a1
Michael Salzarulo <50628598+mikesalzarulo@users.noreply.github.com>
2021-07-06 16:21:22 -0400

    jQuery
    Adding ID selectors with jQuery to chose a button randomly and animate.

--- Simon Game Challenge/game.js
+++ Simon Game Challenge/game.js
@@ -1,12 +1,18 @@
-var gamePattern = [];
-
-function nextSequence() {
-    var randomNumber = Math.floor((Math.random() * 4));
-    var randomChosenColour = buttonColours[randomNumber];
-};
 
 var buttonColours = ["red", "blue", "green", "yellow"];
 
-gamePattern.push(randomChosenColour);
+var gamePattern = [];
 
-$("#randomChosenColour").
+function nextSequence() {
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+   
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
+}
