11eece31afd1d4c0a9313f5954f5e4be673c986d
Michael Salzarulo <50628598+mikesalzarulo@users.noreply.github.com>
2021-07-17 13:54:50 -0400

    Finished Game
    If wrong error function and end game along with restarting game. jQuery, JavaScript.

--- Simon Game Challenge/game.js
+++ Simon Game Challenge/game.js
@@ -23,45 +23,35 @@ $(".btn").click(function() {
   playSound(userChosenColour);
   animatePress(userChosenColour);
 
-  
   checkAnswer(userClickedPattern.length-1);
 });
 
-
-
 function checkAnswer(currentLevel) {
 
-    
     if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-
-      console.log("success");
-
-      
       if (userClickedPattern.length === gamePattern.length){
-
-        
         setTimeout(function () {
           nextSequence();
         }, 1000);
-
       }
-
     } else {
+      playSound("wrong");
+      $("body").addClass("game-over");
+      $("#level-title").text("Game Over, Press Any Key to Restart");
 
-      console.log("wrong");
+      setTimeout(function () {
+        $("body").removeClass("game-over");
+      }, 200);
 
+      startOver();
     }
-
 }
 
+
 function nextSequence() {
-
-  
   userClickedPattern = [];
-
   level++;
   $("#level-title").text("Level " + level);
-
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
@@ -70,14 +60,20 @@ function nextSequence() {
   playSound(randomChosenColour);
 }
 
-function playSound(name) {
-  var audio = new Audio("sounds/" + name + ".mp3");
-  audio.play();
-}
-
 function animatePress(currentColor) {
   $("#" + currentColor).addClass("pressed");
   setTimeout(function () {
     $("#" + currentColor).removeClass("pressed");
   }, 100);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
 }
